2cec96cc58f1ba79d3da5f7b72242a26590a2c38
anishgulati2 <theanishgulati@gmail.com>
2024-06-09 16:28:04 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,9 @@ export function App() {
         <Header />
 
         <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example app",
-              url: "https://example.com",
-            }}
-          />
+          <ConnectButton client={client}/>
         </div>
 
-        <ThirdwebResources />
       </div>
     </main>
   );
@@ -37,9 +30,7 @@ function Header() {
       />
 
       <h1 className="text-2xl md:text-6xl font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-violet-500"> vite </span>
+        Mintify
       </h1>
 
       <p className="text-zinc-300 text-base">
@@ -53,45 +44,3 @@ function Header() {
   );
 }
 
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=vite-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
-  );
-}
--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,10 @@ import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const clientId = import.meta.env.VITE_THIRD_WEB_CLIENT_ID;
+const secretKey = import.meta.env.VITE_THIRD_WEB_SECRET_KEY;
 
 export const client = createThirdwebClient({
   clientId: clientId,
+  secretKey: secretKey,
 });
